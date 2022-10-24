31146fc304d13c952d6133d3712512efb9401896
Maxi Crimi <maxicrini@gmail.com>
2022-10-24 18:01:36 -0300

    card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Navegacion from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-import Card from './components/MangaObj';
-import Home from './components/Home';
+import Card from './components/Card';
+
 
 function App() {
   return (
     <div>
       <Navegacion/>
       <ItemListContainer msg="Bienvenido a mi Page"/>
-     
       <Card/>
     </div>
   );
--- src/components/MangaObj.js
+++ src/components/MangaObj.js
@@ -149,21 +149,5 @@ const manga = [
 
 
 
-const Card = () => {
-    return(
-        <div className='containerCard'>
-            {manga.map((manga,i) =>(
-                <div className='card' key={i}>
-                    <h3>{`Saga: ${manga.saga}`}</h3>
-                    <h4>{`Tomo: ${manga.tomo}`}</h4>
-                    <img src={manga.imagen}/>
-                    <p>{`$ ${manga.precio}`}</p>
-                    <Button/>
-                </div>
-            ))}            
-        </div>
-    )
-}
 
-
-export default Card;
+export default manga;
